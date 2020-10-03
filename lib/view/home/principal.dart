43d731363892b9806f4aeb9ac35c7485f298f2aa
Ramon Paolo Maran <ramonpaolomaran12@gmail.com>
2020-10-02 21:36:20 -0300

    Add RefreshIndicator

--- lib/view/home/principal.dart
+++ lib/view/home/principal.dart
@@ -9,6 +9,9 @@ import 'package:hurryAgro/view/home/anuncio.dart';
 //---- Datas
 import 'package:hurryAgro/data/data.dart';
 
+import '../../data/data.dart';
+import '../../data/data.dart';
+
 class Principal extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
@@ -17,81 +20,90 @@ class Principal extends StatefulWidget {
 class _HomeState extends State<Principal> {
   var textTitulo = TextStyle(fontSize: 20);
   var textPreco = TextStyle(fontSize: 20, color: Colors.blue);
+
+  Future getDados() async {
+    print("Recaregado");
+    return anuncios;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-        child: Column(children: [
-      Divider(
-        color: Colors.white,
-      ),
-      Image.asset(
-        "imagens/logoNome.png",
-        height: 100,
-      ),
-      CarouselSlider(
-        items: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(20),
-            child: Image.network(
-              "https://cdn.pixabay.com/photo/2020/09/22/09/09/leaf-5592392__340.jpg",
-            ),
+    return RefreshIndicator(
+        child: SingleChildScrollView(
+            child: Column(children: [
+          Divider(
+            color: Colors.white,
           ),
-          ClipRRect(
-            borderRadius: BorderRadius.circular(20),
-            child: Image.network(
-              "https://cdn.pixabay.com/photo/2014/11/28/00/07/mushrooms-548360__340.jpg",
-            ),
+          Image.asset(
+            "imagens/logoNome.png",
+            height: 100,
           ),
-          ClipRRect(
-            borderRadius: BorderRadius.circular(20),
-            child: Image.network(
-              "https://cdn.pixabay.com/photo/2020/06/05/16/53/zucchini-5263781__340.jpg",
-            ),
+          CarouselSlider(
+            items: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.network(
+                  "https://cdn.pixabay.com/photo/2020/09/22/09/09/leaf-5592392__340.jpg",
+                ),
+              ),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.network(
+                  "https://cdn.pixabay.com/photo/2014/11/28/00/07/mushrooms-548360__340.jpg",
+                ),
+              ),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.network(
+                  "https://cdn.pixabay.com/photo/2020/06/05/16/53/zucchini-5263781__340.jpg",
+                ),
+              ),
+            ],
+            options: CarouselOptions(
+                enlargeCenterPage: true,
+                autoPlay: true,
+                viewportFraction: 0.4, //Padding estre as imagens
+                autoPlayCurve: Curves.ease,
+                enableInfiniteScroll: true,
+                height: 130,
+                pauseAutoPlayOnTouch: true,
+                onPageChanged: (index, b) => print(index)),
           ),
-        ],
-        options: CarouselOptions(
-            enlargeCenterPage: true,
-            autoPlay: true,
-            viewportFraction: 0.4, //Padding estre as imagens
-            autoPlayCurve: Curves.ease,
-            enableInfiniteScroll: true,
-            height: 130,
-            pauseAutoPlayOnTouch: true,
-            onPageChanged: (index, b) => print(index)),
-      ),
-      Container(
-          width: 1000,
-          height: 400,
-          child: ListView.builder(
-            itemCount: anuncios.length,
-            itemBuilder: (context, index) {
-              return GestureDetector(
-                  onTap: () => Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => Anuncio(
-                                  name: anuncios[index]["name"],
-                                  price: anuncios[index]["price"],
-                                  image: anuncios[index]["image"],
-                                )),
-                      ),
-                  child: ClipRRect(
-                      borderRadius: BorderRadius.circular(120),
-                      child: Container(
+          Container(
+              width: 1000,
+              height: 500,
+              child: ListView.builder(
+                itemCount: anuncios.length,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                      onTap: () => Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => Anuncio(
+                                      name: anuncios[index]["name"],
+                                      price:
+                                          anuncios[index]["price"].toString(),
+                                      image: anuncios[index]["image"],
+                                    )),
+                          ),
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.circular(120),
                           child: Card(
                               child: ListTile(
-                        title: Text(anuncios[index]["name"]),
-                        subtitle: Text("R\$${anuncios[index]["price"]}"),
-                        leading: ClipRRect(
-                          child: Image.asset(
-                            anuncios[index]["image"],
-                            height: 100,
-                            fit: BoxFit.fill,
-                          ),
-                        ),
-                      )))));
-            },
-          ))
-    ]));
+                            title: Text(anuncios[index]["name"]),
+                            subtitle: Text("R\$${anuncios[index]["price"]}"),
+                            leading: ClipRRect(
+                              borderRadius: BorderRadius.circular(20),
+                              child: Image.asset(
+                                anuncios[index]["image"],
+                                width: 80,
+                                height: 80,
+                              ),
+                            ),
+                          ))));
+                },
+              ))
+        ])),
+        onRefresh: () => getDados());
   }
 }
