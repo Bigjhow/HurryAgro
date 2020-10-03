644c7612d923adacc4e3fec37d55df540f82c058
Ramon Paolo Maran <ramonpaolomaran12@gmail.com>
2020-10-03 17:12:57 -0300

    Funcional agora

--- lib/view/user/meusAnuncios.dart
+++ lib/view/user/meusAnuncios.dart
@@ -1,16 +1,8 @@
 //---- Packages
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:carousel_slider/carousel_slider.dart';
-
-//---- Screens
-import 'package:hurryAgro/view/home/anuncio.dart';
 
 //---- Datas
 import 'package:hurryAgro/data/data.dart';
-
-import '../../data/data.dart';
-import '../../data/data.dart';
 
 class MeusAnuncios extends StatefulWidget {
   @override
@@ -18,61 +10,103 @@ class MeusAnuncios extends StatefulWidget {
 }
 
 class _HomeState extends State<MeusAnuncios> {
-  var textTitulo = TextStyle(fontSize: 20);
-  var textPreco = TextStyle(fontSize: 20, color: Colors.blue);
-
   Future getDados() async {
     print("Recaregado");
     return anuncios;
   }
 
+  var produtoApagado;
+
   @override
   Widget build(BuildContext context) {
-    appBar : AppBar(
-        backgroundColor: Colors.green,
-      );
-    return RefreshIndicator(
-        child: SingleChildScrollView(
-            child: Column(children: [
-          Divider(
-            color: Colors.white,
-          ),
-          Image.asset(
-            "imagens/logoNome.png",
-            height: 100,
-          ),
-          Container(
-              width: 1000,
-              height: 500,
-              child: ListView.builder(
-                itemCount: anuncios.length,
-                itemBuilder: (context, index) {
-                  return GestureDetector(
-                      onTap: () =>{},
-                      child: ClipRRect(
-                          borderRadius: BorderRadius.circular(120),
-                          child: Card(
-                              child: ListTile(
-                            title: Text(anuncios[index]["name"]),
-                            subtitle: Text("R\$${anuncios[index]["price"]}"),
-                            leading: ClipRRect(
-                              borderRadius: BorderRadius.circular(20),
-                              child: Image.asset(
-                                anuncios[index]["image"],
-                                width: 80,
-                                height: 80,
-                              ),
-                            ),
-                            trailing: IconButton(
-                              icon: Icon(Icons.delete),
-                              onPressed :(){
-                                anuncios.remove([index]);
-                              }
-                            ), 
-                          ))));
-                },
-              ))
-        ])),
-        onRefresh: () => getDados());
+    return Scaffold(
+        appBar: AppBar(),
+        body: FutureBuilder(
+          future: getDados(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return RefreshIndicator(
+                  child: SingleChildScrollView(
+                      child: Column(children: [
+                    Divider(
+                      color: Colors.white,
+                    ),
+                    Image.asset(
+                      "imagens/logoNome.png",
+                      height: 100,
+                    ),
+                    Container(
+                        width: 1000,
+                        height: 500,
+                        child: ListView.builder(
+                          itemCount: anuncios.length,
+                          itemBuilder: (context, index) {
+                            return Dismissible(
+                                direction: DismissDirection.startToEnd,
+                                background: Container(
+                                  color: Colors.red,
+                                  child: Align(
+                                      alignment: Alignment(-0.9, 0),
+                                      child: Icon(Icons.delete,
+                                          color: Colors.white)),
+                                ),
+                                onDismissed: (d) {
+                                  produtoApagado = anuncios[index];
+                                  setState(() {
+                                    anuncios.removeAt(index);
+                                  });
+                                  Scaffold.of(context).showSnackBar(SnackBar(
+                                    content: Text("O Produto: " +
+                                        produtoApagado["name"] +
+                                        " foi excluido"),
+                                    action: SnackBarAction(
+                                        label: "Desfazer",
+                                        onPressed: () {
+                                          setState(() {
+                                            anuncios.insert(
+                                                index, produtoApagado);
+                                          });
+                                        }),
+                                    duration: Duration(seconds: 3),
+                                    backgroundColor: Colors.white,
+                                  ));
+                                },
+                                key: Key(DateTime.now().toString()),
+                                child: GestureDetector(
+                                    onTap: () => {},
+                                    child: ClipRRect(
+                                        borderRadius:
+                                            BorderRadius.circular(120),
+                                        child: Card(
+                                            child: ListTile(
+                                          title: Text(anuncios[index]["name"]),
+                                          subtitle: Text(
+                                              "R\$${anuncios[index]["price"]}"),
+                                          leading: ClipRRect(
+                                            borderRadius:
+                                                BorderRadius.circular(20),
+                                            child: Image.asset(
+                                              anuncios[index]["image"],
+                                              width: 80,
+                                              height: 80,
+                                            ),
+                                          ),
+                                          trailing: IconButton(
+                                              icon: Icon(Icons.delete),
+                                              onPressed: () {
+                                                setState(() {
+                                                  anuncios.removeAt(index);
+                                                });
+                                              }),
+                                        )))));
+                          },
+                        ))
+                  ])),
+                  onRefresh: () => getDados());
+            } else if (snapshot.connectionState == ConnectionState.waiting) {
+              return CircularProgressIndicator();
+            }
+          },
+        ));
   }
 }
