9ca96ef0e0a0f2ba3ec1a3838837275320e5c279
Diego <jhowzeraDiego@gmail.com>
2020-10-03 03:27:52 -0300

    add list usuarios

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -2,3 +2,6 @@ List anuncios = [
   {"name": "Pepino", "price": 24.99, "image": "imagens/tomate.jpg","describe" : "Pepino do sul cultivado na alemanha pelo garnde querissimo KID BENGALA" },
   {"name": "Tomate", "price": 3.99, "image": "imagens/tomate.jpg","describe" : "TOMATE do sul cultivado na alemanha pelo garnde querissimo KID BENGALA"},
 ];
+List usuarios = [
+  {"email":"diego@gmail.com","senha":"hurry"},
+];
