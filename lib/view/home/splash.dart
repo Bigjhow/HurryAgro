abbc855c189fd7e8d68884f6db955ce15a465e9d
Ramon Paolo Maran <ramonpaolomaran12@gmail.com>
2020-10-06 12:18:46 -0300

    Add timer

--- lib/view/home/splash.dart
+++ lib/view/home/splash.dart
@@ -10,71 +10,67 @@ class Splash extends StatefulWidget {
   _State createState() => _State();
 }
 
-class _State extends State<Splash> {  
+class _State extends State<Splash> {
   @override
-  void initState(){
+  void initState() {
     super.initState();
-    Timer.periodic(Duration(seconds: 10),(timer) {
-        setState(() {
-          button = RaisedButton(
-                onPressed: () {
-                  route();
-                },
-                child: Text(
-                  "Login",
-                  style: TextStyle(
-                      color: Colors.white, 
-                      fontFamily: "Noto Sans JP",
-                      ),
-                ),
-                color: Colors.green,
-    );
+    Timer.periodic(Duration(seconds: 10), (timer) {
+      setState(() {
+        button = RaisedButton(
+          onPressed: () {
+            route();
+          },
+          child: Text(
+            "Login",
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: "Noto Sans JP",
+            ),
+          ),
+          color: Colors.green,
+        );
+      });
+      timer.cancel();
     });
-     });
   }
+
   Widget button = CircularProgressIndicator(
-              backgroundColor: Colors.blue,
-              strokeWidth: 2,
-            );
+    backgroundColor: Colors.blue,
+    strokeWidth: 2,
+  );
 
+  route() {
+    Navigator.pushReplacement(context,
+        PageTransition(child: Login(), type: PageTransitionType.bottomToTop));
+  }
 
-
- route() {
-   Navigator.pushReplacement(context, 
-    PageTransition(                       
-       child: Login(),
-       type: PageTransitionType.bottomToTop)
-   );
- }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.green,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              child: Image.asset(
-                "imagens/logo.png",
-                height: 300,
-                
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            child: Image.asset(
+              "imagens/logo.png",
+              height: 300,
+            ),
+          ),
+          Container(
+            child: Padding(
+              padding: EdgeInsets.all(15),
+              child: Text(
+                "O app Hurry Agro conta com funções como a criação de anúncios de venda, com controle total ao criador, como descrição, preço, localidade, também é possível iniciar um chat com o vendedor, para que assim negociem a venda. A HurryAgro somente dá espaço para os anunciantes, não fica apar das logísticas referente a vendas, apenas do anuncio e conversação via chat.",
+                textAlign: TextAlign.center,
               ),
             ),
-            Container(
-              child: Padding(
-                padding: EdgeInsets.all(15),
-                child: Text(
-                  "O app Hurry Agro conta com funções como a criação de anúncios de venda, com controle total ao criador, como descrição, preço, localidade, também é possível iniciar um chat com o vendedor, para que assim negociem a venda. A HurryAgro somente dá espaço para os anunciantes, não fica apar das logísticas referente a vendas, apenas do anuncio e conversação via chat.",
-                  textAlign: TextAlign.center,
-                ),),
-              
-            ),
-            Padding(padding: EdgeInsets.only(top: 20)),
-            button,
-          ],
-          )
-      ),
+          ),
+          Padding(padding: EdgeInsets.only(top: 20)),
+          button,
+        ],
+      )),
     );
   }
 }
